6ea30bc388c7cc629e58b5ba19d09a32ce76add0
zqz-git <105975409+zqz-git@users.noreply.github.com>
2025-08-20 05:52:42 +0800

    Add files via upload

---
+++ js/myresume.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    // window.location.replace(window.location.href);
+    setTimeout(() => {
+            window.location.href = "../index.html"
+        }, 30000)
+        //     window.location.href = "../index.html"
+}
